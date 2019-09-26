69b509de834575694539fba2dda5f98017060d6f
Alan Dodge <alphaechododge@gmail.com>
2019-09-26 18:18:56 +0000

    Refactored breedFetcher and added some unit tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,18 @@
 const request = require('request');
 
-const catReq = function(catBreed) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (!(error === null)) {
-      console.log('error:', error); // Print the error if one occurred
-      return;
+      return callback(error, null);
     }
 
     if (body === "[]") {
-      console.log(`${catBreed} is not a breed of cat.`);
+      callback(`${breedName} is not a breed of cat.`, null);
     } else {
       const data = JSON.parse(body);
-      console.log(data[0].description);
+      callback(null, data[0].description);
     }
   });
 };
 
-const args = process.argv.slice(2);
-catReq(args[0]);
+module.exports = { fetchBreedDescription };
